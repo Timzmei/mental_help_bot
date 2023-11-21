a2a3bc6718891e5bd0c8c87aeb75972767e0a0cf
Timur Guliev <mail@guliev-timur.ru>
2023-11-22 01:43:48 +0500

    add

--- script.js
+++ script.js
@@ -144,20 +144,20 @@ document.getElementById("submit").addEventListener("click", function () {
 });
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
-    selectedAnswers = [];
+    // selectedAnswers = [];
 
-    for (let i = 1; i <= questions_count; i++) {
-        const selectedValue = document.querySelector(`input[name="q${i}"]:checked`);
+    // for (let i = 1; i <= questions_count; i++) {
+    //     const selectedValue = document.querySelector(`input[name="q${i}"]:checked`);
 
-        if (!selectedValue) {
-            selectedAnswers.push({ question: `Вопрос ${i}`, answer: 'Ответ не выбран' });
-        } else {
-            const questionText = document.querySelector(`.question:nth-child(${i}) p`).textContent;
-            const answerText = selectedValue.parentElement.textContent.trim();
-            selectedAnswers.push({ question: questionText, answer: answerText });
-        }
-    }
-    selectedAnswers.push({ test_name: fullTestName, result: totalScore, text_result: resultText });
+    //     if (!selectedValue) {
+    //         selectedAnswers.push({ question: `Вопрос ${i}`, answer: 'Ответ не выбран' });
+    //     } else {
+    //         const questionText = document.querySelector(`.question:nth-child(${i}) p`).textContent;
+    //         const answerText = selectedValue.parentElement.textContent.trim();
+    //         selectedAnswers.push({ question: questionText, answer: answerText });
+    //     }
+    // }
+    // selectedAnswers.push({ test_name: fullTestName, result: totalScore, text_result: resultText });
     tg.sendData(selectedAnswers);
     Telegram.WebApp.close();
 
