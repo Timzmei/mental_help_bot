8ed215df52d50fabbf320f66cd4ac42087cee76a
Timur Guliev <mail@guliev-timur.ru>
2023-12-11 01:08:26 +0500

    изменен шрифт и оформление pdf

--- get_test.py
+++ get_test.py
@@ -63,7 +63,7 @@ def create_pdf(test_data, answers_array, result_test, from_user_username, from_u
         
         for line in lines:
             c.drawString(x_position, ypos, line)
-            ypos -= 15
+            ypos -= 20
             # y_position -= 20
             if y_position < 50 and ypos < 50:
                 c.showPage()
@@ -71,7 +71,7 @@ def create_pdf(test_data, answers_array, result_test, from_user_username, from_u
                 c.setFont(font, font_size)
                 y_position = 750
                 c.drawString(100, y_position, line)
-            y_position -= 15
+            y_position -= 20
     
     
     # Добавляем информацию о тестируемом, лечащем враче и клинике
@@ -162,7 +162,129 @@ def create_pdf(test_data, answers_array, result_test, from_user_username, from_u
     
     c.save()
     print(f"PDF-файл с результатами теста создан: {pdf_filename}")
+    
+    
+def create_pdf_2(test_data, answers_array, result_test, from_user_username, from_user_id, test_name, user_name, doc_name):
+    now = datetime.now()
+    current_date = now.strftime("%d.%m.%Y")
+    
+    pdf_filename = "Результаты теста.pdf"
+    
+    c = canvas.Canvas(pdf_filename, pagesize=letter)
+    # Устанавливаем цвет шрифта    
+    y_position = 750  # Начальная позиция на странице
+    
+    # Добавление информации на страницу
+    def add_info(text, x_position, ypos, font, font_size, font_color):
+        nonlocal y_position
+        c.setFillColor(font_color)
+        c.setFont(font, font_size)
+        width, height = letter
 
+        lines = simpleSplit(text, font, 12, width)
+        print(lines)
+        
+        for line in lines:
+            c.drawString(x_position, ypos, line)
+            ypos -= 20
+            # y_position -= 20
+            if y_position < 50 and ypos < 50:
+                c.showPage()
+                c.setFillColor(font_color)
+                c.setFont(font, font_size)
+                y_position = 750
+                c.drawString(100, y_position, line)
+            y_position -= 20
+    
+    
+    # Добавляем информацию о тестируемом, лечащем враче и клинике
+    add_info(f"{test_name}", 180, y_position, "DejaVu-Bold", 15, black)
+    y_position -=50
+    add_info(f"Имя тестируемого:", 45, y_position, "DejaVu-Bold", 10, black)
+    y_position += 20
+    add_info(f"{user_name}", 180, y_position, "DejaVu", 9, black)
+    c.rect(170, y_position + 15, 250, 18)  # Координаты и размеры рамки
+    
+    # Добавление линий для разделения блоков информации
+    # c.line(45, y_position - 40, 550, y_position - 40)
+    # c.line(45, y_position - 70, 550, y_position - 70)
+
+    add_info(f"Лечащий врач:", 45, y_position, "DejaVu-Bold", 10, black)  # Поле для заполнения
+    y_position += 20
+    add_info(f"{doc_name}", 180, y_position, "DejaVu", 9, black)
+    c.rect(170, y_position + 15, 250, 18)  # Координаты и размеры рамки
+    # Добавляем информацию о тесте и результатах
+    add_info(f"Дата прохождения:", 45, y_position, "DejaVu-Bold", 10, black)
+    y_position += 20
+    add_info(f"{current_date}", 180, y_position, "DejaVu", 9, black)
+    # add_info(f"Название теста:", 45, y_position, "DejaVu-Bold", 10, black)
+    y_position += 20
+  
+    # Добавляем логотип клиники
+    c.drawImage('logo.png', 45, 710, width=94, height=41)
+       
+    add_info("Результаты теста:", 45, y_position, "DejaVu-Bold", 10, black)
+    
+    for scale, score in result_test.items():
+        add_info(f"{scale}:", 45, y_position, "DejaVu-Bold", 10, black)
+        y_position += 20
+        add_info(f"{score}", 180, y_position, "DejaVu", 9, blue)
+        
+    # Добавление линий для разделения результатов
+    # c.line(45, y_position - 5, 550, y_position - 5)
+            
+    # Добавление вопросов и ответов из answersArray    
+    print(answers_array)
+    for i, answer_dict in enumerate(answers_array, start=1):
+        print(f'i={i} : answer_dict={answer_dict}')
+        question_number = int(answer_dict['question'].split()[1]) - 1
+        
+        print(f"answer_dict = {answer_dict}")
+        if test_name == 'Опросник гипомании HCL 32':
+            sections_number = int(answer_dict['section'])
+            section = test_data["sections"][sections_number]
+            print(f'sec= {section}')
+            question = section["questions"][question_number]
+            question_text = question["question"]
+            if question["type"] == "text":
+                answer_text = answer_dict['answer']
+            else:
+                answer_index = int(answer_dict['answer'])
+                for ans in question["answers"]:
+                    print(f"ans = {ans}")
+                    # print(f"ans['text'] = {ans.text}")
+                    if ans["value"] == answer_index:
+                        answer_text = ans["text"]
+                        print(f"answer_text = {answer_text}")
+                    # answer_text = question["answers"][answer_index]["text"]
+            if question_number + 1 == 1:
+                add_info(f"{section['section']}", 70, y_position, "DejaVu-Bold", 9, black)
+            add_info(f"Вопрос {question_number + 1}: {question_text}", 70, y_position, "DejaVu-Bold", 8, blue)
+            add_info(f"Ответ: {answer_text}", 100, y_position, "DejaVu-Italic", 8, black)
+        else:
+            question = test_data["questions"][question_number]
+            
+            question_text = question["question"]
+            answer_index = int(answer_dict['answer'])
+            for ans in question["answers"]:
+                print(f"ans = {ans}")
+                # print(f"ans['text'] = {ans.text}")
+                if ans["value"] == answer_index:
+                    answer_text = ans["text"]
+                    print(f"answer_text = {answer_text}")
+                # answer_text = question["answers"][answer_index]["text"]
+            add_info(f"Вопрос {i}: {question_text}", 70, y_position, "DejaVu-Bold", 8, blue)
+            add_info(f"Ответ: {answer_text}", 100, y_position, "DejaVu-Italic", 8, black)
+        # Добавление линий между каждым вопросом и ответом для лучшей разбивки
+        # c.line(45, y_position - 5, 550, y_position - 5)
+    
+    # # Добавление общего балла (GSI), индекс PSI и индекс PDSI
+    # add_info(f"Общий балл (GSI): {total_score}", y_position)
+    # add_info(f"Индекс PSI: {psi_count}", y_position)
+    # add_info(f"Индекс PDSI: {pdsi}", y_position)
+    
+    c.save()
+    print(f"PDF-файл с результатами теста создан: {pdf_filename}")
 def get_test_data(test_name):
     # Путь к вашему JSON файлу с тестовыми данными
     json_file_path = f'{test_name}.json'
@@ -344,7 +466,7 @@ async def get_answer(web_app_message):
 
     
     
-    pdf_file = create_pdf(file_data, answers_array, result_test, from_user_username, from_user_id, full_test_name, user_name, doc_name)
+    pdf_file = create_pdf_2(file_data, answers_array, result_test, from_user_username, from_user_id, full_test_name, user_name, doc_name)
 
     # Выводим информацию о тесте
     print(f"Название теста: {full_test_name}")
